c7070ed6319fdbbbcb6f04bff303aec717baef70
same-hack <samehack.info@gmail.com>
2023-01-30 21:08:12 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-app';
+  myText = 'サメハックです！';
+
+  /**
+   * 入力欄が更新されるたびに呼び出される
+   * eventとしてmyTextの値が渡される
+   */
+  onChangeText(event: string) {
+    console.log(event);
+  }
 }
